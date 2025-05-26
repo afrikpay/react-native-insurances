66c39121e8eee21f88c93c0f3026ffe0a97067a0
patson120 <patrickkennenl@gmail.com>
2025-05-26 12:12:40 +0100

    Mise en place de la pagge de de détail d'une souscription

--- src/screens/detail-souscription.tsx
+++ src/screens/detail-souscription.tsx
@@ -1,10 +1,10 @@
-import { Image, ScrollView, StatusBar, Text, TouchableOpacity, View } from 'react-native'
+import { ScrollView, StatusBar, Image, Text, TouchableOpacity, View } from 'react-native'
 import * as Icon from "react-native-feather"
 import RenderHtml from 'react-native-render-html'
 import { COLORS } from '../constants/Colors'
-import { ImageSante } from '../constants/Images'
 import { height, width } from '../constants/size'
 import Navigation from '../services/Navigation'
+import { Box } from '../components/ui/Box'
 
 
 export default function DetailSouscription() {
@@ -24,21 +24,61 @@ export default function DetailSouscription() {
                 </TouchableOpacity>
                 <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Détails souscription</Text>
             </View>
-            <View style={{ flexDirection: 'row', alignItems: 'center'}}>
-                <Text style={{ flex: 1, fontSize: 16, fontWeight: 'bold' }}>Assurance santé classique</Text>
-                <Image
-                    alt="Image de l'assurance santé"
-                    source={ImageSante}
-                    style={{
-                        height: 45,
-                        width: 45,
-                    }}
-                />
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <Text style={{ fontSize: 22, fontWeight: 'bold', color: COLORS.primary }}>Premium Gold</Text>
+                <TouchableOpacity onPress={() => { }} style={{
+                    width: 40, height: 40,
+                    borderRadius: 20, backgroundColor: COLORS.primary,
+                    justifyContent: 'center', alignItems: 'center'
+                }}>
+                    <Icon.MessageSquare color={COLORS.white} strokeWidth={2} width={20} height={20} />
+                </TouchableOpacity>
             </View>
+            <Box width={'100%'} padding={18}>
+                <View style={{ flexDirection: 'row', gap: 3 }}>
+                    <View style={{ flex: 1, gap: 16}}>
+                        <Image
+                            alt="Image de l'assurance santé"
+                            source={require("../assets/logo.png")}
+                            style={{ height: 40, width: 40 }}
+                        />
+                        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 8 }}>
+                            <View style={{ height: 10, width: 10, backgroundColor: COLORS.success, borderRadius: 10 }}></View>
+                            <Text style={{ color: COLORS.success, fontSize: 16 }}>Actif</Text>
+                        </View>
+                        <Text style={{ fontSize: 16, fontWeight: 'bold'}}>01 an</Text>
+                    </View>
+                    <View style={{ flex: 3, flexDirection: 'column', gap: 8 }}>
+                        <View style={{ flexDirection:"row", justifyContent: 'space-between'}}>
+                            <Text style={{ fontSize: 12, fontWeight: 'bold', color: COLORS.dark }}>Type:</Text>
+                            <Text numberOfLines={2} ellipsizeMode='tail' style={{ fontSize: 14, fontWeight: 'bold', color: COLORS.primary }}>Assurance santé classique</Text>
+                        </View>
+                        <View style={{ flexDirection:"row", justifyContent: 'space-between'}}>
+                            <Text style={{ fontSize: 12, fontWeight: 'bold', color: COLORS.dark }}>Souscrit le :</Text>
+                            <Text numberOfLines={2} ellipsizeMode='tail'>25/04/2025</Text>
+                        </View>
+                        <View style={{ flexDirection:"row", justifyContent: 'space-between'}}>
+                            <Text style={{ fontSize: 12, fontWeight: 'bold', color: COLORS.dark }}>Activé le :</Text>
+                            <Text numberOfLines={2} ellipsizeMode='tail'>27/04/2025</Text>
+                        </View>
+                        <View style={{ flexDirection:"row", justifyContent: 'space-between'}}>
+                            <Text style={{ fontSize: 12, fontWeight: 'bold', color: COLORS.dark }}>Validité :</Text>
+                            <Text numberOfLines={2} ellipsizeMode='tail'>26/04/2026</Text>
+                        </View>
+                        <View style={{ flexDirection:"row", justifyContent: 'space-between'}}>
+                            <Text style={{ fontSize: 12, fontWeight: 'bold', color: COLORS.dark }}>Prime: </Text>
+                            <Text numberOfLines={2} ellipsizeMode='tail' style={{ fontSize: 14, fontWeight: 'bold' }}>74 000 XAF</Text>
+                        </View>
+                    </View>
+                </View>
+            </Box>
+            <TouchableOpacity onPress={() => { }} style={{ }}>
+                <Text style={{ color: COLORS.primary, fontSize: 12, fontWeight: 'bold' }}>Télécharger le contrat</Text>
+            </TouchableOpacity>
         </View>
         <ScrollView
             showsVerticalScrollIndicator={false}
-            style={{ flex: 1, padding: 20, backgroundColor: '#F4F5F6'}}>   
+            style={{ flex: 1, padding: 20, backgroundColor: '#F4F5F6'}}>  
             <RenderHtml
                 contentWidth={width}
                 source={{
