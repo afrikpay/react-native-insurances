12b1af5705598de4bc02f70d6c1cb07b079db83d
patson120 <patrickkennenl@gmail.com>
2025-06-24 10:04:43 +0100

    Mise à jour du formulaire souscription

--- src/screens/forms/souscription-form.tsx
+++ src/screens/forms/souscription-form.tsx
@@ -1,49 +1,35 @@
 import { useEffect, useState } from 'react'
-import { ActivityIndicator, FlatList, Image, Pressable, ScrollView, StatusBar, Text, TouchableOpacity, View } from 'react-native'
+import { ActivityIndicator, Pressable, ScrollView, StatusBar, Text, TouchableOpacity, View } from 'react-native'
 import * as Icon from "react-native-feather"
 import { Modal, Portal } from 'react-native-paper'
 import StepFormBuilder from '../../components/form/StepFormBuilder'
 import type { FormStep } from '../../components/form/types/types'
 import { COLORS } from '../../constants/Colors'
-import { IMAGES } from '../../constants/Images'
 import { height, width } from '../../constants/size'
 import { apiClient } from '../../data/axios'
 import Navigation from '../../services/Navigation'
-import WebviewScreen from './components/WebviewScreen'
-
-const operateursMobile: Record<string, any>[]  = [
-    {
-        id: 1,
-        name: 'MTN Money Cameroun',
-        logo: IMAGES['mtnMoney'],
-        slug: 'mtn_mobile_money'
-    },
-    {
-        id: 2,
-        name: 'Orange Money Cameroun',
-        logo: IMAGES['orangeMoney'],
-        slug: 'orange_money'
-    }
-]
-
+import SimpleToast from 'react-native-simple-toast'
 
 export default function SouscriptionForm(props: any) {
 
     const { planId, insurerId } = props.route.params;
 
     const [visible, setVisible] = useState(false);
-    const [showPaiementMothod, setShowPaiementMothod] = useState(false);
 
     const showModal = () => setVisible(true);
     const hideModal = () => setVisible(false);
 
     const [loading, setLoading] = useState(false)
+    const [savingData, setSavingData] = useState(false)
+
+
     const [formResult, setFormResult] = useState<Record<string, any>>()
     const [sectionFields, setSectionFields] = useState<Record<string, any>[]>([])
 
     const [formStep, setFormStep] = useState<FormStep[]>([])
     const [formStepCopy, setFormStepCopy] = useState<FormStep[]>([])
     const [assures, setAssures] = useState<Record<string, any>[]>([])
+    const [defaultValues, setDefaultValues] = useState<any>(null)
 
     useEffect(() => {
         (async () => {
@@ -54,8 +40,7 @@ export default function SouscriptionForm(props: any) {
                     insurerId: insurerId,
                     page: 1
                 }
-                const response: any = await apiClient.post('/secure/mobile/form/v1', {...data});       
-                // console.log(JSON.stringify(response, null, 2));
+                const response: any = await apiClient.post('/secure/mobile/form/v1', {...data});
                 setFormResult(response.result)
                 setSectionFields(response.result.fields)
 
@@ -77,7 +62,6 @@ export default function SouscriptionForm(props: any) {
                             })),
                         ],
                         onStepComplete(data: any) {
-                            console.log('data', data);
                             return Promise.resolve(data);
                         },
                     };
@@ -94,52 +78,16 @@ export default function SouscriptionForm(props: any) {
         })()
     }, []);
 
-    const paymentUrl = ''
-
-    const successPayment = () => {
-
-    }
-    const failedPayment = () => {
-        
-    }
-    const cancelPayment = () => {
-        
-    }
-
     const addInsurer = (formData: Record<string, any>) => {
         // Ajouter l'utilisateur à la liste des assurés
-        setAssures(prev => ([...prev, formData]))
+        setAssures(prev => ([formData, ...prev,]))
         setFormStep([])
-    }
-
-    const handleFetchPaymentUrl = async () => {
-        try {
-            const data = {
-                referenceNumber: "willistowerwatson:0c972b67-d852-4b55-8d70-056b7fcfaf50",
-                amount: 20000,
-                externalId: "184595625878",
-                paymentWallet: "237652310829",
-                data: {
-                  insurerId: 1,
-                  paymentService: "mtn-money-ecommerce-payment-service-feature"
-                }
-            }
-            const response: any = await apiClient.post('/secure/mobile/subscription/v1', {...data},
-                { "Service": "mtn-money-ecommerce-payment-service-feature" })
-
-            console.log(JSON.stringify(response, null, 2))
-        }
-        catch (error) {
-            console.error('Error saving data:', error);
-        }
-        finally{
-            // setLoading(false);
-        }  
+        setDefaultValues(null)
     }
 
     const handleSubmitForm =  async() => {
-        if (loading) return
-        setLoading(true);
+        if (savingData) return
+        setSavingData(true);
         
         try {
             const data = {
@@ -150,13 +98,14 @@ export default function SouscriptionForm(props: any) {
             }
             const response: any = await apiClient.post('/secure/mobile/subscription/v1', {...data})
             setFormResult(response.result)
-            setShowPaiementMothod(true)
+            SimpleToast.show("Souscription effectuée avec succès!", 5)
         }
-        catch (error) {
+        catch (error: any) {
             console.error('Error saving data:', error);
+            SimpleToast.show(`Error saving data: ${error.message}`, 5)
         }
         finally{
-            setLoading(false);
+            setSavingData(false);
         }
     }
 
@@ -174,6 +123,16 @@ export default function SouscriptionForm(props: any) {
             }
         }
         return data
+    }
+
+    const deleteInsurer = (insurer: Record<string, any>) => {
+        setAssures(prev => (prev.filter(p => p.nom !== insurer.nom)))
+    }
+
+    const editInsurer = (insurer: Record<string, any>) => {
+        setFormStep(formStepCopy)
+        setDefaultValues(insurer)
+        deleteInsurer(insurer)
     }
 
     return (
@@ -203,12 +162,12 @@ export default function SouscriptionForm(props: any) {
                     )
                 }
                 { 
-                    formStep.length > 0  &&
+                    (formStep.length > 0 || defaultValues)  &&
                     <View>
                         <StepFormBuilder
                             onSubmit={addInsurer}
                             steps={formStep}
-                            defaultValues={{}}
+                            defaultValues={{...defaultValues}}
                             externalValues={{}}
                             onError={console.error}
                             onExternalValueChange={console.warn}
@@ -235,8 +194,8 @@ export default function SouscriptionForm(props: any) {
                                 }}>
                                     <Text style={{ color: COLORS.primary}}>#{index+1} {insurer.nom}</Text>
                                     <View style={{ flexDirection: 'row', justifyContent: 'center', gap: 10 }}>
-                                        <Icon.Edit2 style={{ width: 7, height: 7, borderColor: COLORS.gray }} />
-                                        <Icon.Trash2 style={{ width: 7, height: 7, borderColor: COLORS.danger }} />
+                                        <Icon.Edit2 onPress={() => editInsurer(insurer)} style={{ width: 7, height: 7, borderColor: COLORS.gray }} />
+                                        <Icon.Trash2 onPress={() => deleteInsurer(insurer)} style={{ width: 7, height: 7, borderColor: COLORS.danger }} />
                                     </View>
                                 </View>
                                )) 
@@ -250,51 +209,18 @@ export default function SouscriptionForm(props: any) {
                             </Pressable>
                             <Pressable
                                 onPress={handleSubmitForm}
-                                style= {{ paddingVertical: 10, width: 150, backgroundColor: COLORS.primary, borderRadius: 100 }}>
+                                style= {{ paddingVertical: 10, width: 150, backgroundColor: COLORS.primary, borderRadius: 100,
+                                    flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: 8
+                                }}>
+                                {
+                                    savingData && 
+                                    <ActivityIndicator size={'small'} color={COLORS.white} style={{ height: 20, width: 20 }} />
+                                }
                                 <Text style={{ color: COLORS.white, fontWeight: "bold", textAlign: "center"}}>Souscrire</Text>
                             </Pressable>
                         </View>
                     </View>
                 }
-                {
-                    showPaiementMothod &&
-                    <View style={{ flexDirection: 'column', gap: 12, marginTop: 20, paddingHorizontal: 20 }}>
-                        <Text style={{ flex: 1, fontSize: 18, fontWeight: 'bold' }}>Payer ma souscription avec:</Text>
-                        <FlatList
-                            data={operateursMobile}
-                            showsHorizontalScrollIndicator={false}
-                            horizontal
-                            extraData={(item: any) => `${item.id}`}
-                            renderItem={({ item }) => (
-                                <Pressable
-                                        onPress={showModal}
-                                        key={item.id} style={{
-                                        width: 80, height: 80, borderWidth: 0.05,
-                                        borderRadius: 12,
-                                        marginRight: 15,
-                                        padding: 5, gap: 15,
-                                        backgroundColor: COLORS.white, // Ajout d'une couleur de fond pour l'ombre
-                                        shadowColor: COLORS.dark, // Couleur de l'ombre
-                                        shadowOffset: { width: 0, height: 4 }, // Décalage de l'ombre
-                                        shadowOpacity: 0.2, // Opacité de l'ombre
-                                        shadowRadius: 6, // Rayon de flou de l'ombre
-                                        elevation: 2, // Ombre pour Android
-                                    }}>
-                                        <View style={{width: '100%', height: '100%', overflow: 'hidden', borderRadius: 10}}>
-                                            <Image
-                                                alt="Image de l'assurance santé"
-                                                source={item.logo}
-                                                style={{ 
-                                                    width: '100%',
-                                                    height: '100%'
-                                                }}
-                                            />
-                                        </View>
-                                </Pressable>
-                            )}
-                        />
-                    </View>
-                }  
 
                 <View style={{ height: 80, width: '100%'}} />
             </ScrollView>
@@ -303,12 +229,9 @@ export default function SouscriptionForm(props: any) {
              <Portal>
                 <Modal visible={visible} onDismiss={hideModal} 
                     contentContainerStyle={{backgroundColor: 'white', padding: 20, width: '90%', marginLeft: '5%', borderRadius: 10}}>
-                    <WebviewScreen
-                        paymentUrl={paymentUrl ?? ''}
-                        onPaymentSuccess={successPayment}
-                        onPaymentFailed={failedPayment}
-                        onPaymentCancel={cancelPayment}
-                    />
+                    <>
+
+                    </>
                 </Modal>
             </Portal> 
         </View>
