f45666af08bdda14c7092380a48600b1db188e29
patson120 <patrickkennenl@gmail.com>
2025-07-11 17:57:55 +0100

    Update

--- src/screens/infos.tsx
+++ src/screens/infos.tsx
@@ -25,6 +25,41 @@ export default function Infos() {
         <ScrollView
             showsVerticalScrollIndicator={false}
             style={{ flex: 1, paddingHorizontal: 20, marginTop: 15 }}>
+            
+            <Text style={{ fontWeight: "bold", fontSize: 20, marginBottom: 20 }}>À propos de l'application</Text>
+            <Text style={{ lineHeight: 18 }}>
+                Bienvenue sur votre espace assurance, 
+                l'application d'assurance mobile conçue spécifiquement 
+                pour répondre à votre besoin. Nous simplifions le processus 
+                de souscription et de gestion de vos assurances, 
+                vous offrant tranquillité d'esprit et protection 
+                en quelques clics.
+            </Text>
+            <Text style={{ fontWeight: "bold", fontSize: 20, marginTop: 20, marginBottom: 12 }}>Notre Mission</Text>
+            <Text style={{ lineHeight: 18, paddingBottom: 30, borderBottomWidth: 0.33, borderBottomColor: COLORS.gray }}>
+                Notre mission est de rendre l'assurance accessible, 
+                transparente et rapide pour tous. 
+                Nous croyons que la protection de votre avenir et 
+                de vos biens ne devrait pas être compliquée.
+            </Text>
+            <Text style={{ fontWeight: "bold", fontSize: 20, marginTop: 20, marginBottom: 12 }}>Comment ça marche ?</Text>
+            <Text style={{ lineHeight: 18, }}>
+                Avec votre espace assurance, souscrire à une assurance est simple et rapide.
+            </Text>
+            <Text style={{ lineHeight: 18, marginTop: 8 }}>
+                1) - Choisissez votre produit d'assurance : Explorez notre gamme de produits (auto, santé, habitation, etc.) adaptés au marché camerounais.
+            </Text>
+            <Text style={{ lineHeight: 18,  marginTop: 8 }}>
+                2) - Obtenez un devis instantané : Entrez vos informations pour recevoir un devis personnalisé immédiatement.
+            </Text>
+            <Text style={{ lineHeight: 18,  marginTop: 8 }}>
+                3) - Souscrivez en ligne : Payez en toute sécurité via des méthodes de paiement locales (Mobile Money, cartes bancaires) et recevez votre police d'assurance numériquement.
+            </Text>
+            <Text style={{ lineHeight: 18,  marginTop: 8 }}>
+                4) - Gérez vos polices : Accédez à vos documents, suivez vos remboursements et gérez vos sinistres directement depuis l'application.
+            </Text>
+            <View style={{ marginTop: 30}} />
+
             {/* 
                 <View style={{ width: '100%', borderRadius: 12, backgroundColor: COLORS.primary, padding: 15}}>
                     <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: 22}}>
@@ -88,39 +123,6 @@ export default function Infos() {
                     </TouchableOpacity>
                 </View>
             */}
-            <Text style={{ fontWeight: "bold", fontSize: 20, marginBottom: 20 }}>À propos de l'application</Text>
-            <Text style={{ lineHeight: 18 }}>
-                Bienvenue sur votre espace assurance, 
-                l'application d'assurance mobile conçue spécifiquement 
-                pour répondre à votre besoin. Nous simplifions le processus 
-                de souscription et de gestion de vos assurances, 
-                vous offrant tranquillité d'esprit et protection 
-                en quelques clics.
-            </Text>
-            <Text style={{ fontWeight: "bold", fontSize: 20, marginTop: 20, marginBottom: 12 }}>Notre Mission</Text>
-            <Text style={{ lineHeight: 18, paddingBottom: 30, borderBottomWidth: 0.33, borderBottomColor: COLORS.gray }}>
-                Notre mission est de rendre l'assurance accessible, 
-                transparente et rapide pour tous. 
-                Nous croyons que la protection de votre avenir et 
-                de vos biens ne devrait pas être compliquée.
-            </Text>
-            <Text style={{ fontWeight: "bold", fontSize: 20, marginTop: 20, marginBottom: 12 }}>Comment ça marche ?</Text>
-            <Text style={{ lineHeight: 18, }}>
-                Avec votre espace assurance, souscrire à une assurance est simple et rapide.
-            </Text>
-            <Text style={{ lineHeight: 18, marginTop: 8 }}>
-                1) - Choisissez votre produit d'assurance : Explorez notre gamme de produits (auto, santé, habitation, etc.) adaptés au marché camerounais.
-            </Text>
-            <Text style={{ lineHeight: 18,  marginTop: 8 }}>
-                2) - Obtenez un devis instantané : Entrez vos informations pour recevoir un devis personnalisé immédiatement.
-            </Text>
-            <Text style={{ lineHeight: 18,  marginTop: 8 }}>
-                3) - Souscrivez en ligne : Payez en toute sécurité via des méthodes de paiement locales (Mobile Money, cartes bancaires) et recevez votre police d'assurance numériquement.
-            </Text>
-            <Text style={{ lineHeight: 18,  marginTop: 8 }}>
-                4) - Gérez vos polices : Accédez à vos documents, suivez vos remboursements et gérez vos sinistres directement depuis l'application.
-            </Text>
-            <View style={{ marginTop: 30}} />
         </ScrollView>
     </SafeAreaView>
   )
