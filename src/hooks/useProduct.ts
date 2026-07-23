65f468011091587ea00867682aa3a46fd6c68dd2
patson120 <patrickkennenl@gmail.com>
2026-07-23 09:07:08 +0100

    update

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -7,7 +7,7 @@ export default function useProduct(){
     const [products, setProducts] = useState<ProduitAssurance[]>([]);
     const client = useFetchClient()
     const findProducts = async () => {
-        const response: any = await client.fetch("/secure/mobile/categories/v1", );
+        const response: any = await client.fetch("secure/mobile/categories/v1", );
         console.log("response", JSON.stringify(response, null, 2));
         setProducts(response.result ?? ([] as ProduitAssurance[]));
     }
