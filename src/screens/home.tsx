b7ff92b83625ec25232e3aa780755754bef8a2fa
patson120 <patrickkennenl@gmail.com>
2026-08-11 15:55:38 +0100

    Update

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -47,7 +47,7 @@ export default function Home() {
     <SafeAreaView
       style={{
         flex: 1,
-        width: width,
+        width: "100%",
         paddingHorizontal: 20,
         backgroundColor: COLORS.white,
         flexDirection: 'column',
@@ -318,7 +318,7 @@ export function ProductSection(
           </View>
         )}
         {products.slice(0, 4).map((product: any, index: number) => (
-          <Box key={index} width={width / 2 - 24} padding={10}>
+          <Box key={index} width={width / 2 - 26} padding={10}>
             <Pressable
               onPress={() => { Navigation.navigate(ROUTES.ASSUREURS, { product }); }}
               style={{ flexDirection: 'row', alignItems: 'center', gap: 8 }}>
