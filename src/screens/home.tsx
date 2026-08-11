f59191c9ba2cc00785b044ad3fdc00ded269d38b
patson120 <patrickkennenl@gmail.com>
2026-08-11 17:47:20 +0100

    Update

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -316,7 +316,7 @@ export function ProductSection(
           </View>
         )}
         {products.slice(0, 4).map((product: any, index: number) => (
-          <Box key={index} width={width / 2.38} padding={10}>
+          <Box key={index} width={width / 2.34} padding={10}>
             <Pressable
               onPress={() => { Navigation.navigate(ROUTES.ASSUREURS, { product }); }}
               style={{ flexDirection: 'row', alignItems: 'center', gap: 8 }}>
