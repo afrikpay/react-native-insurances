0c806aba677a7d02aef5d0e76fd904b8fdd5861c
patson120 <patrickkennenl@gmail.com>
2026-08-11 17:53:24 +0100

    Update

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -48,6 +48,7 @@ export default function Home() {
       style={{
         flex: 1,
         width: "100%",
+        height: "100%",
         paddingHorizontal: 20,
         backgroundColor: COLORS.white,
         flexDirection: 'column',
